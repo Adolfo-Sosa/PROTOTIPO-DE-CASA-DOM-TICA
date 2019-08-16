909e0f390968aead34e4e72fbcc6eebf688d12a6
bruno09miranda <50469366+bruno09miranda@users.noreply.github.com>
2019-08-16 13:32:54 -0500

    Add files via upload

---
+++ ENCENDIDO DE LED AL RECIVIR UNA SEÑAL DE CONTROL INFRARROJO.ino
@@ -0,0 +1,21 @@
+#include <IRremote.h>
+
+int RECV_PIN = 8; 
+IRrecv irrecv(RECV_PIN); 
+decode_results results; 
+
+void setup() {  
+irrecv.enableIRIn(); 
+pinMode(9, OUTPUT);
+}
+
+boolean on = LOW;
+
+void loop() { 
+if (irrecv.decode(&results)) { 
+  on = !on;
+  digitalWrite(9, on? HIGH : LOW);
+irrecv.resume(); 
+delay(300); 
+} 
+}
