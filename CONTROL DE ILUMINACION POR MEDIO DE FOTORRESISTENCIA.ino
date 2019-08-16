2f5a43b228b83c751c6bb639d212df4036366bc6
bruno09miranda <50469366+bruno09miranda@users.noreply.github.com>
2019-08-16 13:34:28 -0500

    Add files via upload

---
+++ CONTROL DE ILUMINACION POR MEDIO DE FOTORRESISTENCIA.ino
@@ -0,0 +1,20 @@
+#define pinrele 11
+boolean rele=false;
+int ldr;
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(pinrele,OUTPUT);
+}
+
+void loop()
+{
+  ldr=analogRead(0);
+  if(ldr>=800)rele=true;
+  else if(ldr<=600)rele=false;
+  digitalWrite(pinrele,rele);
+  digitalWrite(13,rele);
+  Serial.print(ldr);
+  delay;
+}
+
